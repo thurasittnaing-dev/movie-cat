c46782b2d019fd984710d5fa05794867afea3da3
Thura Sitt Naing <coding2am@gmail.com>
2025-09-11 18:16:47 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,137 @@
+import React, {useEffect, useState} from 'react'
+import Search from "./components/Search.jsx";
+import Loading from "./components/Loading.jsx";
+import MoviceCard from "./components/MoviceCard.jsx";
+import {useDebounce} from "react-use";
+import {getTrendingMovies, updateSearchCount} from "./lib/appwrite.js";
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept : 'application/json',
+        Authorization: `Bearer ${API_KEY}`
+    }
+}
+
+const App = () => {
+
+    const [searchTerm,setSearchTerm] = useState('');
+    const [debounceSearchTerm,setDebounceSearchTerm] = useState('');
+    const [errorMessage,setErrorMessage] = useState(null);
+    const [movieList,setMovieList] = useState([]);
+    const [isLoading,setIsLoading] = useState(false);
+    const [trendingMovies,setTrendingMovies] = useState([]);
+
+    useDebounce(()=> setDebounceSearchTerm(searchTerm),500,[searchTerm]);
+
+    const fetchMovies = async (query = '')=> {
+        setIsLoading(true);
+        setErrorMessage(null);
+
+        try {
+            const endPoint = query ?
+                `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+            :
+                `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+            const response = await fetch(endPoint,API_OPTIONS);
+
+            if(!response.ok) {
+                throw new Error('Failed to fetch movies');
+            }
+
+            const data = await response.json();
+
+            if(data.Response === 'False') {
+                setErrorMessage(data.Error || 'failed to fetch movie');
+                setMovieList([]);
+                return;
+            }
+
+
+            setMovieList(data.results || []);
+            
+            if(query && data.results.length > 0) {
+                await updateSearchCount(query,data.results[0]);
+            }
+        }catch (error) {
+            setMovieList([]);
+            setIsLoading(false);
+            console.log('Movie fetch error!', error);
+            setErrorMessage('Error fetching movies. Please try again later.');
+        }finally {
+            setIsLoading(false);
+        }
+    }
+
+    const loadTrendingMovies = async () => {
+        try {
+            const movies = await getTrendingMovies();
+
+            setTrendingMovies(movies);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies(debounceSearchTerm);
+      
+    }, [debounceSearchTerm]);
+
+    useEffect(() => {
+        loadTrendingMovies();
+    }, []);
+
+    return (
+        <main>
+            <div className="pattern"/>
+ 
+            <div className="wrapper">
+                <header>
+                    <img src="./public/logo.png" alt="Logo" />
+                    <h1>Movie Cat <span className="text-gradient">Streamline</span> your movie journey 🚀</h1>
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                </header>
+
+                {
+                    trendingMovies.length > 0 && (
+                        <section className="trending">
+                            <h2>Tending Movies</h2>
+                            <ul>
+                                {
+                                    trendingMovies.map((movie,index) =>(
+                                        <li key={movie.$id}>
+                                            <p>{index + 1}</p>
+                                            <img src={movie.poster_url} alt={movie.title} />
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </section>
+                    )
+                }
+
+                <section className="all-movies">
+                    <h2>All Movies</h2>
+                    {
+                        isLoading ? (
+                            <Loading />
+                        ) : errorMessage ? (
+                            <p className="text-red-500">{errorMessage}</p>
+                        ) : (
+                            <ul>
+                                {movieList.map((movie) => (
+                                   <MoviceCard key={movie.id} movie={movie} />
+                                ))}
+                            </ul>
+                        )
+                    }
+                </section>
+            </div>
+        </main>
+    )
+}
+export default App
---
+++ src/lib/appwrite.js
@@ -0,0 +1,72 @@
+import {Client,Databases, ID, Query} from "appwrite";
+
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DB_ID;
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const END_POINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
+
+const client = new Client()
+    .setEndpoint(END_POINT)
+    .setProject(PROJECT_ID);
+
+const databases = new Databases(client);
+
+export const updateSearchCount = async (searchTerm,movie) => {
+
+    try {
+        const result = await databases.getDocument({
+            databaseId: DATABASE_ID,
+            collectionId: 'metrics',
+            documentId: '',
+            queries: [
+                Query.equal('searchTerm',searchTerm),
+            ] 
+        });
+        
+        console.log(result);
+
+        if(result.documents.length > 0) {
+            const doc = result.documents[0];
+            await databases.updateDocument({
+                databaseId: DATABASE_ID,
+                collectionId: 'metrics',
+                documentId: doc.$id,
+                data: {
+                    count : doc.count + 1,
+                }, // optional
+            });
+        }else {
+            await databases.createDocument({
+                databaseId: DATABASE_ID,
+                collectionId: 'metrics',
+                documentId: ID.unique(),
+                data: {
+                    searchTerm,
+                    count : 1,
+                    movie_id : movie.id,
+                    poster_url: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                },
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getTrendingMovies = async () => {
+    try {
+        const result = await databases.getDocument({
+            databaseId: DATABASE_ID,
+            collectionId: 'metrics',
+            documentId: '',
+            queries: [
+                Query.limit(5),
+                Query.orderDesc('count')
+            ] 
+        });
+
+        return result.documents;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
